69468dd597c436cc095fe8b38f28f33f84f99236
process255 <sean@process255.com>
2012-03-29 19:49:00 -0600

    working through the walkthrough and adding images

--- Dev-Denver/Dev-Denver/DDSpeakerViewController.h
+++ Dev-Denver/Dev-Denver/DDSpeakerViewController.h
@@ -1,11 +1,3 @@
-//
-//  DDViewController.h
-//  Dev-Denver
-//
-//  Created by Sean Dougherty on 3/29/12.
-//  Copyright (c) 2012 process255, LLC. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface DDSpeakerViewController : UIViewController
